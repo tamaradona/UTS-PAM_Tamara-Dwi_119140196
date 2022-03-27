b41c74353ee5ff411ea2a27c3845dd12555cd9a2
Tamara Dwi R <100558933+tamaradona@users.noreply.github.com>
2022-03-27 10:40:10 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Navigation from './screen/navigation'
+  
+export default function App() {
+      return (
+          <Navigation/>
+      );
+  }
